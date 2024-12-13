7f2d248a5b328d01d5d3fcdce59d7e6789bb8b38
HamBeomJoon <hbj0802@naver.com>
2024-12-13 16:44:31 +0900

    feat(Constants): 상수 메세지 추가

---
+++ src/main/kotlin/oncall/utils/Constants.kt
@@ -0,0 +1,9 @@
+package oncall.utils
+
+object Constants {
+    const val INPUT_MONTH_START_DAY = "비상 근무를 배정할 월과 시작 요일을 입력하세요> "
+    const val INPUT_WEEKDAY_WORKERS = "평일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
+    const val INPUT_HOLIDAY_WORKERS = "휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> "
+
+    const val ERROR_MESSAGE = "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요."
+}
