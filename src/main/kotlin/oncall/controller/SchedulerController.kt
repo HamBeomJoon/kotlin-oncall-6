2c51a618c29f11a4f8c98e780d844ad7c5e51b7a
HamBeomJoon <hbj0802@naver.com>
2024-12-13 13:46:32 +0900

    feat(SchedulerController): 공휴일 리스트 정의

--- src/main/kotlin/oncall/controller/SchedulerController.kt
+++ src/main/kotlin/oncall/controller/SchedulerController.kt
@@ -3,6 +3,8 @@ package oncall.controller
 import camp.nextstep.edu.missionutils.Console
 
 class SchedulerController {
+    private val holidayList =
+        mapOf(Pair(1, 1), Pair(3, 1), Pair(5, 5), Pair(6, 6), Pair(8, 15), Pair(10, 3), Pair(10, 9), Pair(12, 25))
 
     fun start() {
         print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
