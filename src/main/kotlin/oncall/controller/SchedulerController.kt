c582555604952b166edbd903aeac77d34a584f0c
HamBeomJoon <hbj0802@naver.com>
2024-12-13 13:46:59 +0900

    feat(SchedulerController): 해당 달 몇일까지 있는지 정의

--- src/main/kotlin/oncall/controller/SchedulerController.kt
+++ src/main/kotlin/oncall/controller/SchedulerController.kt
@@ -5,6 +5,10 @@ import camp.nextstep.edu.missionutils.Console
 class SchedulerController {
     private val holidayList =
         mapOf(Pair(1, 1), Pair(3, 1), Pair(5, 5), Pair(6, 6), Pair(8, 15), Pair(10, 3), Pair(10, 9), Pair(12, 25))
+    private val dayOfMonth = mapOf(
+        Pair(1, 31), Pair(3, 31), Pair(5, 31), Pair(7, 31), Pair(8, 31), Pair(10, 31), Pair(12, 31),
+        Pair(4, 30), Pair(6, 30), Pair(9, 30), Pair(1, 30), Pair(2, 28)
+    )
 
     fun start() {
         print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
