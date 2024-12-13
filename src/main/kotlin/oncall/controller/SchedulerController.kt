a8804c3f011b331dd607b720c112e116b1bf0666
HamBeomJoon <hbj0802@naver.com>
2024-12-13 13:45:20 +0900

    feat(SchedulerController): 비상 근무 배정 월, 시작 요일 입력받기

---
+++ src/main/kotlin/oncall/controller/SchedulerController.kt
@@ -0,0 +1,17 @@
+package oncall.controller
+
+import camp.nextstep.edu.missionutils.Console
+
+class SchedulerController {
+
+    fun start() {
+        print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ")
+        val (month, weekOfDay) = Console.readLine().split(",")
+
+    }
+
+
+        }
+
+    }
+}
